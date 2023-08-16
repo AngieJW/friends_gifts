ff9044c06a1cd6e4ddf5a229975df72607651575
Angie Duhard <angie.duhard@gmail.com>
2023-08-16 23:56:07 +0200

    added json package

--- src/Controller/GiftController.php
+++ src/Controller/GiftController.php
@@ -11,8 +11,19 @@ use Symfony\Component\HttpFoundation\Request;
 class GiftController extends AbstractController
 {
     #[Route('/gift', name: 'app_gift')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+      $session = $request->getSession();
+      if (!$session->has('gifts')) {
+        $gifts = [
+          'clothes' => 'clothes',
+          'shoes' => 'shoes',
+          'toys' => 'toys',
+          'books' => 'books',
+        ];
+        $session->set('gifts', $gifts);
+      }
+
         return $this->render('gift/index.html.twig', [
             'controller_name' => 'GiftController',
         ]);
