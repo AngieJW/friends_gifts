e2596199e80db3d542b373ba8e0e8a35f9026ddd
Angie Duhard <angie.duhard@gmail.com>
2023-08-17 00:55:37 +0200

    added gift add and flash messages

--- src/Controller/GiftController.php
+++ src/Controller/GiftController.php
@@ -22,10 +22,31 @@ class GiftController extends AbstractController
           'books' => 'books',
         ];
         $session->set('gifts', $gifts);
+        $this->addFlash('info', 'Your gift list has been created');
       }
 
         return $this->render('gift/index.html.twig', [
             'controller_name' => 'GiftController',
         ]);
+    }
+
+    #[Route('/gift/add/{name}/{content}', name: 'app_gift.add')]
+    public function addGift(Request $request, $name, $content): Response
+    {
+      $session = $request->getSession();
+      if ( $session->has('gifts') ) {
+        $gifts = $session->get('gifts');
+        if (isset($gifts[$name])) {
+          $this->addFlash('error', "Your gift $name already exists");
+        } else {
+          $gifts[$name] = $content;
+          $session->set('gifts', $gifts);
+          $this->addFlash('info', 'Your gift has been added');
+        }
+      } else {
+        $this->addFlash('error', 'Your gift list does not exist');
+      }
+
+      return $this->redirectToRoute('app_gift');
     }
 }
